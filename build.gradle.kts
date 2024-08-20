276c4c187ee7ef44abf85b79b27f614d1e106086
Gabor Szabo <szgabsz91@gmail.com>
2024-08-20 19:14:40 +0200

    feat: #7 support native builds with GraalVM

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.3.2"
 	id("io.spring.dependency-management") version "1.1.6"
+	id("org.graalvm.buildtools.native") version "0.10.2"
 }
 
 group = "com.github.szgabsz91"
