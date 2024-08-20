0d14ee0e7bb48036a9f0e9d2fec89e642dc3af48
Gabor Szabo <szgabsz91@gmail.com>
2024-08-20 19:14:40 +0200

    feat: #7 support native builds with GraalVM

--- src/main/java/com/github/szgabsz91/foreignlanguagetester/configurations/ForeignLanguageTesterConfiguration.java
+++ src/main/java/com/github/szgabsz91/foreignlanguagetester/configurations/ForeignLanguageTesterConfiguration.java
@@ -2,10 +2,15 @@ package com.github.szgabsz91.foreignlanguagetester.configurations;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.github.szgabsz91.foreignlanguagetester.models.Language;
+import com.github.szgabsz91.foreignlanguagetester.models.TestItem;
+import com.github.szgabsz91.foreignlanguagetester.models.TestItemCounter;
+import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RegisterReflectionForBinding(classes = {Language.class, TestItemCounter.class, TestItem.class})
 public class ForeignLanguageTesterConfiguration {
 
     @Bean
